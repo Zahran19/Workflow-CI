91d3c9f63c7d28ed878b89b3f3f0c182daba3e06
Muhammad Zahran Winzardy <muhammadzahran2004@gmail.com>
2026-05-20 13:21:09 +0700

    Fix absolute pathing for MLflow Docker Build

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,7 +8,7 @@ import shutil
 if __name__ == "__main__":
     print("Mulai melatih model untuk MLProject...")
     
-    # Load dataset hasil preprocessing sebelumnya
+    # Load dataset
     df = pd.read_csv('dataset_clean.csv')
     X = df.drop('loan_status', axis=1)
     y = df['loan_status']
@@ -17,12 +17,12 @@ if __name__ == "__main__":
         model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
         model.fit(X, y)
         
-        # 1. Log model ke MLflow Tracking
-        mlflow.sklearn.log_model(model, "model")
+        # Trik Senior Engineer: Paksa simpan ke root folder GitHub Actions
+        workspace = os.getenv("GITHUB_WORKSPACE", ".")
+        model_path = os.path.join(workspace, "live_model")
         
-        # 2. Simpan model secara lokal ke folder khusus untuk kebutuhan build-docker
-        if os.path.exists("live_model"):
-            shutil.rmtree("live_model")
+        if os.path.exists(model_path):
+            shutil.rmtree(model_path)
             
-        mlflow.sklearn.save_model(model, "live_model")
-        print("Model berhasil dilatih! Artefak disimpan di folder 'live_model'.")
+        mlflow.sklearn.save_model(model, model_path)
+        print(f"Model berhasil dilatih! Artefak aman tersimpan di: {model_path}")
